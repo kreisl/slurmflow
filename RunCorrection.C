56deab59d30a468c85ff993c5466a09bebdcd911
Lukas Kreis <l.kreis@gsi.de>
2019-10-09 16:40:54 +0200

    now working

--- RunCorrection.C
+++ RunCorrection.C
@@ -1,28 +1,37 @@
 // Root macro to start the qvector correction analysis using the AliAnalysisManager
-void RunCorrection(std::string macro, std::string name, int ijob, int nchunks=10, std::string calibfile="correction.root") {
-  std::cout << "starting analysis over " << nchunks << " AOD chunks." << std::endl;  
+void RunCorrection(std::string macro, 
+                   std::string run_list_name, 
+                   int ijob, 
+                   int number_of_chunks, 
+                   std::string correction_file) {
+  std::cout << "starting analysis over " 
+            << number_of_chunks << " AOD chunks." << std::endl;  
   gInterpreter->ProcessLine(".include $ROOTSYS/include");
   gInterpreter->ProcessLine(".include $ALICE_ROOT/include");
   auto mgr = new AliAnalysisManager("AnalysisQn");
   auto handler = new AliAODInputHandler();
   mgr->SetInputEventHandler(handler);
   //Add the multiplicity task to the train.
-  auto multiplicity_task  = reinterpret_cast<AliMultSelectionTask*>(
-      gInterpreter->ExecuteMacro("$ALICE_PHYSICS/OADB/COMMON/MULTIPLICITY/macros/AddTaskMultSelection.C"));
+  auto multiplicity_task = reinterpret_cast<AliMultSelectionTask*>(
+    gInterpreter->ExecuteMacro(
+      "$ALICE_PHYSICS/OADB/COMMON/MULTIPLICITY/macros/AddTaskMultSelection.C"));
 
   //Add the Analysis task to the train.
-  std::string add_task = macro+"(\"Qn\",\""+calibfile+"\")";
-  auto analysis_task = reinterpret_cast<AliAnalysisTaskQn*>(gInterpreter->ExecuteMacro(add_task.data()));
+  std::string add_task = macro+"(\"Qn\",\""+correction_file+"\")";
+  auto analysis_task = reinterpret_cast<AliAnalysisTaskQn*>(
+    gInterpreter->ExecuteMacro(add_task.data()));
   mgr->InitAnalysis();
   
-  // Read files from the runlist at position [ijob*nchunks,ijob+1 * nchunks) 
-  std::ifstream infile(name);
+  // Read files from the runlist at position 
+  // [ijob*number_of_chunks,ijob+1 * number_of_chunks) 
+  std::ifstream infile(run_list_name);
   std::string line;
   int line_counter = 0;
   std::vector<std::string> aod_files;
   if (infile.is_open()) {
     while (std::getline(infile, line)) {
-      if (line_counter >= ijob*nchunks && line_counter < (ijob+1)*nchunks) { 
+      if (line_counter >= ijob*number_of_chunks 
+          && line_counter < (ijob+1)*number_of_chunks) { 
         aod_files.push_back(line);
       }
       line_counter++;
--- correct.sh
+++ correct.sh
@@ -1,64 +1,121 @@
 #!/bin/bash
+#
+# Runs the Qn-Vector corrections using the AliAnalysisManager on a local slurm
+# The corrections are configured in the AddTask macro, which has to be supplied 
+# to the run script.
 
+################################################################################
+# Get absolute path from a supplied relative path
+# Globals:
+#  None
+# Arguments:
+#  $1 relative path
+# Returns:
+#  None
+################################################################################
 function absolute_path() {                                               
   cd "$(dirname "$1")"
   printf "%s/%s\n" "$(pwd)" "$(basename "$1")"
 }
 
+################################################################################
+# Prepends the SLURM settings to the beginning of the .slurm scripts
+# Globals:
+#  slurm_partition
+#  max_job_duration
+# Arguments:
+#  $1 path to the slurm script
+# Returns:
+#  None
+################################################################################
 function prepend_slurm_settings() {
   local slurm_script=$1
   local script_name=$(basename ${slurm_script%.*})
   local new_file=${script_directory}/generated/${script_name}_settings.slurm
   cat - ${slurm_script} <<- HEADER > ${new_file}
-	#!/bin/bash
-	#SBATCH -p ${slurm_partition}
+		#!/bin/bash
+		#SBATCH -p ${slurm_partition}
+		#SBATCH --time=${max_job_duration}
 HEADER
-  # Unused options
-	#SBATCH --time=${max_job_duration}
 }
 
+################################################################################
+# Submits a correction job array and a merging job
+# Globals:
+#  script_directory
+#  output_directory
+#  add_task
+#  run_number
+#  chunks_per_job
+#  task_id_merge
+# Arguments:
+#  $1 run_list 
+#  $2 run_number
+# Returns:
+#  None
+################################################################################
 function submit_job() {
+  local run_list=$1
+  local run_number=$2
+
+  local run_directory=${output_directory}/${run_number}
+  echo $run_directory
   local run_script=$script_directory/generated/create_correction_settings.slurm
   local merge_script=$script_directory/generated/merge_correction_settings.slurm
-  local output_directory=$1
-  local run_list=$2
-  local run_number=$3  
-  local add_task=$4
   local length=$(wc -l < $run_list)
   local modulo=$((length % chunks_per_job))
   local number=$((length / chunks_per_job))
   if (( length % chunks_per_job)); then
     ((number++))
   fi
-  mkdir -p $output_directory/logs
-  mkdir -p $output_directory/data
-  mkdir -p $output_directory/correction
-  correctionpath=$(absolute_path $output_directory/../merged)
-  calibration_file=$correctionpath/correction_${run_number}.root
-  echo $calibration_file
+  mkdir -p ${run_directory}/logs
+  mkdir -p ${run_directory}/data
+  mkdir -p ${run_directory}/correction
+  local correction_file=$output_directory/merged/correction_${run_number}.root
+  echo $correction_file
   local task_id_run=$(sbatch --parsable -J ${run_number} \
-    -o ${output_directory}/logs/slurm-%A_%a.out \
-    -e ${output_directory}/logs/slurm-%A_%a.out --array=1-$number $run_script \
-    $script_directory $output_directory $run_list $chunks_per_job \
-    $calibration_file $add_task)
+    -o ${run_directory}/logs/correct-%A_%a.out \
+    -e ${run_directory}/logs/correct-%A_%a.out --array=1-$number \
+    $run_script $script_directory \
+                $run_directory \
+                $run_list \
+                $chunks_per_job \
+                $correction_file \
+                $add_task \
+                )
   task_id_merge="$task_id_merge:$(sbatch --parsable \
     --dependency afterany:${task_id_run} -J ${run_number}_merge \
-    -o ${output_directory}/logs/slurm-%A_%a.out \
-    -e ${output_directory}/logs/slurm-%A_%a.out ${merge_script} ${output})"
+    -o ${run_directory}/logs/merge-%A.out \
+    -e ${run_directory}/logs/merge-%A.out \
+    ${merge_script} ${run_directory} \
+    )"
 }
 
+################################################################################
+# Submits a correction job array and a merging job
+# Globals:
+#  JOB_CHUNKS
+#  JOB_LENGTH
+#  JOB_PARTITION
+# Arguments:
+#  $1 output directory
+#  $2 input list
+#  $3 path of the add task
+# Returns:
+#  None
+################################################################################
 function main() {
+  local output_directory
+  output_directory=$(absolute_path $1)
+  local input=$2
+  local add_task_path=$3
+
   local chunks_per_job="${JOB_CHUNKS:-80}"
   local max_job_duration="${JOB_LENGTH:-7:00:00}"
   local slurm_partition="${JOB_PARTITION:-debug}"
   local script_directory
   script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" \
     >/dev/null 2>&1 && pwd )"
-
-  echo "Number of chunks per job:" $chunks_per_job 
-  local output_directory=$1
-  local input=$2
-  local add_task_path=$3
   local root_exe=$(which root)
   # Test if root installation available on the lustre
   if [[ ! $root_exe == *"/lustre/"* ]]; then
@@ -67,29 +124,34 @@ function main() {
     echo "aborting"
     exit
   fi
-  prepend_slurm_settings $script_directory/create_correction.slurm
-  prepend_slurm_settings $script_directory/merge_correction.slurm
-  prepend_slurm_settings $script_directory/copy_correction.slurm
+  # Configure scripts with the slurm settings
+  mkdir -p ${script_directory}/generated
+  prepend_slurm_settings ${script_directory}/create_correction.slurm
+  prepend_slurm_settings ${script_directory}/merge_correction.slurm
+  prepend_slurm_settings ${script_directory}/copy_correction.slurm
+  # Create output directories
   mkdir -p ${output_directory}/config
   mkdir -p ${output_directory}/merged
+  # Copies AddTask macro to the output directory
   cp $add_task_path ${output_directory}/config/
-  add_task_name=$(basename $add_task_path)
+  local add_task_name=$(basename $add_task_path)
+  local add_task=$(absolute_path ${output_directory}/config/${add_task_name})
   local merging_ids=""
   while IFS= read line
   do
     echo ${line}
-    file_name=$(basename ${line})
-    run_number=${file_name%%.*}
+    local file_name=$(basename ${line})
+    local run_number=${file_name%%.*}
     mkdir -p ${output_directory}/${run_number}
-    task=$(absolute_path $output_directory/config/${add_task_name})
-    submit_job ${output_directory}/${run_number} ${line} ${run_number} ${task}
+    submit_job ${line} ${run_number}
   done < "$input"
+  # Copies the correction files to the input directory for the next iteration
+  local copy_script=${script_directory}/generated/copy_correction_settings.slurm
   merging_ids=${task_id_merge##:}
   sbatch --dependency afterany:$merging_ids -J copying_correction_files \
-    -o ${output_directory}/merged/slurm-%A.out \
-    -e ${output_directory}/merged/slurm-%A.out \
-    ${script_directory}/generated/copy_correction_settings.slurm \
-    ${output_directory}
+    -o ${output_directory}/merged/copy-%A.out \
+    -e ${output_directory}/merged/copy-%A.out \
+    ${copy_script} ${output_directory}
 }
 
 main "$@"
